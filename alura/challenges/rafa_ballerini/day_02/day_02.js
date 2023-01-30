ed3bb4a31a3cc46a8298bfaa77d01c8be88738da
welderessutti <112294254+welderessutti@users.noreply.github.com>
2023-01-30 18:26:00 -0300

    None

--- alura/challenges/rafa_ballerini/day_02/day_02.js
+++ alura/challenges/rafa_ballerini/day_02/day_02.js
@@ -11,10 +11,25 @@ function dadosPessoa() {
     const idade = document.querySelector("#idade").value;
     const linguagem = document.querySelector("#linguagem").value;
     const resposta = document.querySelector(".resposta");
+    const msg = `Olá ${nome}, você tem ${idade} anos e está estudando ${linguagem}.`;
 
-    resposta.innerHTML = `Olá ${nome}, você tem ${idade} anos e está estudando ${linguagem}.`;
+    resposta.innerHTML = msg;
 
-    alert(`Olá ${nome}, você tem ${idade} anos e está estudando ${linguagem}.`);
+    alert(msg);
+
+    gostaDeEstudar(linguagem);
+}
+
+function gostaDeEstudar(texto) {
+    const resposta_01 = prompt(`Você gosta de estudar ${texto}? Responda 1 para SIM ou 2 para NÃO.`);
+
+    if (resposta_01 == 1) {
+        alert("Muito bom! Continue estudando e você terá muito sucesso.");
+    } else if (resposta_01 == 2) {
+        alert("Ahh que pena... Já tentou aprender outras liguagens?");
+    } else {
+        alert("Resposta inválida!");
+    }
 }
 
 const clicou = document.querySelector("#enviar");
