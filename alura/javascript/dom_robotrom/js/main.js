c1246527adffa8113427aef9d6b317e7c99ca1a9
welderessutti <112294254+welderessutti@users.noreply.github.com>
2023-01-31 18:37:18 -0300

    Continuing DOM Studies

--- alura/javascript/dom_robotrom/js/main.js
+++ alura/javascript/dom_robotrom/js/main.js
@@ -1,27 +1,17 @@
-function manipulandoDados(operacao) {
+function manipulandoDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
     if (operacao === "-") {
-        braco.value = parseInt(braco.value) - 1;
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
 
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
-/*
-somar.addEventListener("click", (evento) => {  // Arrow Function ou Função Anônima.
-    braco.value = parseInt(braco.value) + 1;
-});
-
-subtrair.addEventListener("click", (evento) => {  // Arrow Function ou Função Anônima.
-    braco.value = parseInt(braco.value) - 1;
-});
-*/
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulandoDados(evento.target.textContent);
+        manipulandoDados(evento.target.dataset.controle, evento.target.parentNode);
     });
 });
