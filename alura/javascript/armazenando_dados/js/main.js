aad00482ecd78ead5d139c8ba3daaa5c57e7ddc5
welderessutti <112294254+welderessutti@users.noreply.github.com>
2023-02-08 16:33:47 -0300

    JavaScript LocalStorage Study

--- alura/javascript/armazenando_dados/js/main.js
+++ alura/javascript/armazenando_dados/js/main.js
@@ -1,7 +1,10 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = [];
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach( (elemento) => {
+    criaElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
@@ -9,30 +12,30 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements["nome"];
     const quantidade = evento.target.elements["quantidade"];
 
-    criaElemento(nome.value, quantidade.value);
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 
     nome.value = "";
     quantidade.value = "";
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
     const novoItem = document.createElement("li");
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
-
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual);
-
-    localStorage.setItem("item", JSON.stringify(itens));
 }
