e345b8906aec12bc10b810eb16d01903e046d54e
welderessutti <112294254+welderessutti@users.noreply.github.com>
2023-02-08 23:07:00 -0300

    JavaScript LocalStorage Done

--- alura/javascript/armazenando_dados/js/main.js
+++ alura/javascript/armazenando_dados/js/main.js
@@ -12,14 +12,26 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements["nome"];
     const quantidade = evento.target.elements["quantidade"];
 
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id;
 
-    itens.push(itemAtual);
+        atualizaElemento(itemAtual);
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -33,9 +45,35 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement("strong");
     numeroItem.innerHTML = item.quantidade;
-
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
+
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = "x";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    });
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
