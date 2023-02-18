4a25b53d4011a919993289a7434961ef0aca0fc1
welderessutti <112294254+welderessutti@users.noreply.github.com>
2023-02-18 20:14:20 -0300

    TDD Python

--- alura/python/tdd_testes_unitarios/test_bytebank.py
+++ alura/python/tdd_testes_unitarios/test_bytebank.py
@@ -11,3 +11,12 @@ class TestClass:
         resultado = funcionario_teste.idade()  # When
 
         assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_lucas_carvalho_deve_retornar_carvalho(self):
+        entrada = "  Lucas Carvalho   "  # Given
+        esperado = "Carvalho"
+
+        lucas = Funcionario(entrada, "11/11/2000", 1111)
+        resultado = lucas.sobrenome()  # When
+
+        assert resultado == esperado
