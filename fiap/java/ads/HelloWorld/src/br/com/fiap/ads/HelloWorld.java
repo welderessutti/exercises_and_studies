49165054624cb9e87371b6a70e554b7c188fb52d
Welder Ressutti <welderessutti@gmail.com>
2023-08-16 20:00:15 -0300

    Java

--- fiap/java/ads/HelloWorld/src/br/com/fiap/ads/HelloWorld.java
+++ fiap/java/ads/HelloWorld/src/br/com/fiap/ads/HelloWorld.java
@@ -3,6 +3,20 @@ package br.com.fiap.ads;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		
+		int idade = 34;
+		double preco = 10.5;
+		char sexo = 'M';
+		boolean temFilhos = true;
+		String hello = "Hello, World!";
+		
+		int x = 2000;
+		float y = x;  //Dependendo do tamanho do int, não dá para converter para float.
+		
+		double z = 200.20;
+		int w = (int) z;  //Converter double para int somente com cast e perde informação.
+		
+		System.out.println(hello + temFilhos + sexo + preco + idade);
+		System.out.println(y + w);
 	}
 }
