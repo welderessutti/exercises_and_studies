9e625c92052fdec17e1c6e66ac3301bcd0f0e777
Welder Ressutti <welderessutti@gmail.com>
2023-08-16 18:58:39 -0300

    Java

---
+++ fiap/java/ads/HelloWorld/src/br/com/fiap/ads/HelloWorld.java
@@ -0,0 +1,8 @@
+package br.com.fiap.ads;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ fiap/java/ads/HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author welde
+ *
+ */
+module HelloWorld {
+}
