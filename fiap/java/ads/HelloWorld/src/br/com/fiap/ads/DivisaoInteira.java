ac007f9a5aff328c8ce2f9d9c85da4827afbfe6d
Welder Ressutti <welderessutti@gmail.com>
2023-08-20 21:15:57 -0300

    Java

---
+++ fiap/java/ads/HelloWorld/src/br/com/fiap/ads/DivisaoInteira.java
@@ -0,0 +1,16 @@
+package br.com.fiap.ads;
+
+public class DivisaoInteira {
+
+	public static void main(String[] args) {
+		
+		double x = 7.0;
+		double y = 2.0;
+		
+		int inteiro = (int)(x/y);
+		
+		System.out.println(inteiro);
+
+	}
+
+}
